cf1174d0c5f314dab317d251ffd8e687956a7800
Andranik <andranikaparan@gmail.com>
2022-01-26 15:51:53 +0200

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+        name: "PlayStation 5",
+        imageUrl:
+        "https://content.rozetka.com.ua/goods/images/big/239922397.jpg",
+        description:
+        `Play Has No Limits! Испытайте мощь нового ЦП, графического процессора и SSD с особой встроенной системой ввода и вывода, которые переворачивают представления о возможностях PlayStation. Молниеносная скорость загрузки благодаря сверхскоростному накопителю SSD, невероятный эффект погружения благодаря тактильной отдаче, адаптивным спусковым кнопкам и 3D-звуку, , а также потрясающие игры нового поколения для PlayStation. Наслаждайтесь потрясающей графикой и испытайте новые функции PS5.`,
+        price: 799,
+        countInStock: 15,
+    },
+    {
+        name: "Iphone 12",
+        imageUrl:
+        "https://content2.rozetka.com.ua/goods/images/big/173867674.jpg",
+        description:
+        `Экран (6.1", OLED (Super Retina XDR), 2532x1170) / Apple A14 Bionic / двойная основная камера: 12 Мп + 12 Мп, фронтальная камера: 12 Мп / 256 ГБ встроенной памяти / 3G / LTE / 5G / GPS / Nano-SIM, eSIM / iOS 14`,
+        price: 1099,
+        countInStock: 10, 
+    },
+    {
+        name: "Apple MacBook Pro 14",
+        imageUrl:
+        "https://content.rozetka.com.ua/goods/images/big/248115684.jpg",
+        description:
+        `Экран 14.2 Liquid Retina XDR (3024x1964) 120 Гц, глянцевый / Apple M1 Pro (10 ядер) / RAM 32 ГБ / SSD 4 ТБ / Apple M1 Pro Graphics (16 ядер) / без ОД / Wi-Fi / Bluetooth / веб-камера / macOS Monterey / 1.61 кг / космический серый`,
+        price: 5298,
+        countInStock: 5, 
+    },
+    {
+        name: "Телевизор Sony XR85Z9JCEP",
+        imageUrl:
+        "https://content.rozetka.com.ua/goods/images/big/184575277.jpg",
+        description:
+        `Диапазоны цифрового тюнера
+        DVB-C
+        DVB-S
+        DVB-S2
+        DVB-T
+        DVB-T2`,
+        price: 15000,
+        countInStock: 3, 
+    },
+    {
+        name: "Canon EOS 800D",
+        imageUrl:
+        "https://content2.rozetka.com.ua/goods/images/big/10995260.jpg",
+        description:
+        `Матрица 22.3 x 14.9 мм, 24.2 Мп / объектив EF-S 18-55mm f/4.0-5.6 IS STM / поддержка карт памяти SD/SDHC/SDXC / Сенсорный ЖК-дисплей с переменным углом наклона Clear View II TFT 3" / FullHD-видео / питание от литий-ионного аккумулятора / 131 x 151.4 x 99.9 мм, 737 г`,
+        price: 1233,
+        countInStock: 4, 
+    },
+    {
+        name: "Canon RF 35mm",
+        imageUrl:
+        "https://content2.rozetka.com.ua/goods/images/big/13807781.jpg",
+        description:
+        `Число лепестков диафрагмы
+        9
+        Наименьшее фокусное расстояние
+        35 мм
+        Наибольшее фокусное расстояние
+        35 мм`,
+        price: 400,
+        countInStock: 10, 
+    },
+];
+
+module.exports = products;
